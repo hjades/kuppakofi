084beba876b0163fb7ef9da01ce80e0d52b8279b
Jade Suntoro <hjades@live.com>
2020-12-09 22:01:35 +0800

    add Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
-import { Button } from 'antd';
+import { Layout, Menu } from 'antd';
 import React from 'react';
 import './App.css';
 
 interface AppProps {}
 
+const { Header, Content, Footer } = Layout;
+
 function App({}: AppProps) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Button className="App-link" type="primary" danger loading>
-          Button
-        </Button>
-      </header>
-    </div>
+    <Layout>
+      <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+        <div />
+        <Menu theme="dark" mode="horizontal">
+          <Menu.Item key="1">nav 1</Menu.Item>
+          <Menu.Item key="2">nav 2</Menu.Item>
+        </Menu>
+      </Header>
+      <Content style={{ padding: '0 50px', marginTop: 64 }}>
+        <div style={{ padding: 24, minHeight: 380 }}>Content</div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>
+        KuppaKofi ©2020 Created by Jade with ♥️
+      </Footer>
+    </Layout>
   );
 }
 
